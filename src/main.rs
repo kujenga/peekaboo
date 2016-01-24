294bbcf831bee76274aa2520539e5437a07603fd
kujenga <ataylor0123@gmail.com>
2016-01-24 09:33:03 -0500

    image returned is a julia fractal

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate iron;
 extern crate router;
 extern crate redis;
 extern crate image;
+extern crate num;
 
 use std::io;
 use iron::prelude::*;
@@ -14,6 +15,7 @@ use redis::Commands;
 use image::{
     ImageBuffer,
 };
+use num::complex::Complex;
 
 fn fetch_an_integer(key: &str, inc: bool) -> redis::RedisResult<i64> {
     // connect to redis
@@ -52,15 +54,10 @@ fn main() {
 
     fn peek_handler(r: &mut Request) -> IronResult<Response> {
         let id = r.extensions.get::<Router>().unwrap().find("id").unwrap();
-        let _ = fetch_an_integer(id, true).unwrap_or(0i64);
+        let count = fetch_an_integer(id, true).unwrap_or(0i64);
 
-        let img = ImageBuffer::from_fn(512, 512, |x, _| {
-            if x % 2 == 0 {
-                image::Luma([0u8])
-            } else {
-                image::Luma([255u8])
-            }
-        });
+        let mut img = ImageBuffer::new(512, 512);
+        apply_julia(&mut img, count);
 
         let content_type = "image/png".parse::<Mime>().unwrap();
         Ok(Response::with((content_type, status::Ok, GenImg{img: img})))
@@ -79,4 +76,40 @@ fn main() {
     router.get("/peek/:id/info", peek_info_handler);
 
 	Iron::new(router).http("localhost:2829").unwrap();
+}
+
+fn apply_julia(img: &mut ImageBuffer<image::Luma<u8>, Vec<u8>>, max_iters: i64) {
+
+    // from: https://github.com/PistonDevelopers/image#62-generating-fractals
+
+    let scalex = 4.0 / img.width() as f32;
+    let scaley = 4.0 / img.height() as f32;
+
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        // if x % 2 == 0 {
+        //     *pixel = image::Luma([0u8])
+        // } else {
+        //     *pixel = image::Luma([255u8])
+        // }
+
+        let cy = y as f32 * scaley - 2.0;
+        let cx = x as f32 * scalex - 2.0;
+
+        let mut z = Complex::new(cx, cy);
+        let c = Complex::new(-0.4, 0.6);
+
+        let mut i = 0;
+
+        for t in 0..max_iters {
+            if z.norm() > 2.0 {
+                break
+            }
+            z = z * z + c;
+            i = t;
+        }
+
+        // Create an 8bit pixel of type Luma and value i
+        // and assign in to the pixel at position (x, y)
+        *pixel = image::Luma([i as u8]);
+    }
 }
